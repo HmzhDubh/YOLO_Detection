0fca21ab85695f23fb72b060913af9ca4c0c7f1e
hmzhdubh <meezo.2015@hotmail.com>
2024-07-12 22:36:05 +0300

    depoling the API of Version 7 training

--- DetectionAPI.py
+++ DetectionAPI.py
@@ -1,27 +1,19 @@
+import os
+from PIL import Image
 from roboflow import Roboflow
-import supervision as sv
-import cv2
-from ultralytics import YOLO
+rf = Roboflow(api_key="add your api key")
+project = rf.workspace().project("name your project ")
+model = project.version(7).model
 
-rf = Roboflow(api_key="aEZAKqe87WUQ22o8TaW8")
-project = rf.workspace().project("food-waste-detection-abkdx")
-model = project.version(1).model
+# infer on a local image
+imgpath = "dataset/image4.jpg"
+# visualize your prediction
+counter = 1
+resultsSavePath = "dataset/API-Detection-results/rImage"+ str(counter)+".jpg"
+print(resultsSavePath)
 
-
-result = model.predict("image2.jpg", confidence=40, overlap=30).json()
-
-labels = [item["class"] for item in result["predictions"]]
-
-detections = sv.Detections.from_roboflow(result)
-
-label_annotator = sv.LabelAnnotator()
-bounding_box_annotator = sv.BoxAnnotator()
-
-image = cv2.imread("image2.jpg")
-
-annotated_image = bounding_box_annotator.annotate(
-    scene=image, detections=detections)
-annotated_image = label_annotator.annotate(
-    scene=annotated_image, detections=detections, labels=labels)
-
-sv.plot_image(image=annotated_image, size=(16, 16))
+while (os.path.exists(resultsSavePath)):
+    counter+=1
+    resultsSavePath = "dataset/API-Detection-results/rImage"+ str(counter)+".jpg"
+print("new file name is : ",resultsSavePath)
+model.predict(imgpath, confidence=40, overlap=30).save(resultsSavePath)
