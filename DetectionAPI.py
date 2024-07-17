4fd6a99026c03afafbf5983931d564313f4cf4f5
hmzhdubh <meezo.2015@hotmail.com>
2024-07-17 10:35:39 +0300

    add method in API file

--- DetectionAPI.py
+++ DetectionAPI.py
@@ -1,19 +1,27 @@
 import os
 from PIL import Image
 from roboflow import Roboflow
-rf = Roboflow(api_key="add your api key")
-project = rf.workspace().project("name your project ")
+
+from picDetect import detection
+
+rf = Roboflow(api_key="secret-key")
+project = rf.workspace().project("plate-waste-detection")
 model = project.version(7).model
 
 # infer on a local image
 imgpath = "dataset/image4.jpg"
 # visualize your prediction
-counter = 1
-resultsSavePath = "dataset/API-Detection-results/rImage"+ str(counter)+".jpg"
-print(resultsSavePath)
-
-while (os.path.exists(resultsSavePath)):
-    counter+=1
+def DetectImgByAPI(img):
+    counter = 1
     resultsSavePath = "dataset/API-Detection-results/rImage"+ str(counter)+".jpg"
-print("new file name is : ",resultsSavePath)
-model.predict(imgpath, confidence=40, overlap=30).save(resultsSavePath)
+    print(resultsSavePath)
+
+    while (os.path.exists(resultsSavePath)):
+        counter+=1
+        resultsSavePath = "dataset/API-Detection-results/rImage"+ str(counter)+".jpg"
+    print("new file name is : ",resultsSavePath)
+    model.predict(imgpath, confidence=40, overlap=30).save(resultsSavePath)
+
+    percResult = detection(resultsSavePath)
+
+print(DetectImgByAPI(imgpath))
